4f219a1f8d3c2d74d4605421e7299b30a2b92d0f
sudharaathith <t.sudharaathith@gmail.com>
2023-09-27 15:03:01 +0530

    summa

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -2,9 +2,17 @@ import React from 'react'
 
 function NavBar() {
   return (
-    <div className='  fixed top-0 w-screen bg-[#15120f] text-orange-700  px-3 py-5 flex flex-row'>
-        Cybrog
-        <div></div>
+    <div className='  fixed top-0 w-screen bg-[#15120f] text-orange-500  px-3 py-5 flex flex-row justify-between items-center'>
+        <div className=' flex flex-row '>
+
+        <img src='/Logo512.png' className=' w-12 h-12' />
+        {/* <div className='my-auto mx-2 text-2xl font-bold font-["Poppin"]'>Cyborgs</div> */}
+        </div>
+        <div className='flex justify-stretch gap-5 mx-3'>
+         <div>Event</div>
+         <div>About</div>
+         <div>Contact</div>
+        </div>
     </div>
   )
 }

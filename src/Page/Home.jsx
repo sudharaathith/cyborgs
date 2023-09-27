8eb7c51f33a96cc3739a7fdfbdafbb366bf9aeb0
sudharaathith <t.sudharaathith@gmail.com>
2023-09-27 14:20:21 +0530

    summa

--- src/Page/Home.jsx
+++ src/Page/Home.jsx
@@ -3,13 +3,8 @@ import { motion } from 'framer-motion'
 
 function Home() {
   return (
-    <div className=' w-screen h-screen'>
-         <motion.div
-        initial={{ y: 0, x: 0 }}
-        animate={{ y: -10, x: -10 }}
-        transition={{ repeat: Infinity, duration: 10, repeatType: 'reverse' }}
-        className="w-full h-full bg-gradient-to-b from-blue-500 via-blue-400 to-blue-300"
-      />
+    <div className=' w-screen h-screen bg-[#181402]'>
+         
     </div>
   )
 }

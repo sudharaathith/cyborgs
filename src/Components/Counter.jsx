4fb7442b549d584c5d3abfedc8f48e78818738f0
sudharaathith <t.sudharaathith@gmail.com>
2023-09-27 21:45:21 +0530

    summa

--- src/Components/Counter.jsx
+++ src/Components/Counter.jsx
@@ -1,30 +1,82 @@
-import React from 'react'
-import Countdown from 'react-countdown'
-import { motion } from 'framer-motion'
+import { AnimatePresence, motion } from "framer-motion";
+import { useEffect, useRef, useState } from "react";
 
-function Counter(props) {
+// NOTE: Change this date to whatever date you want to countdown to :)
+const COUNTDOWN_FROM = "10/21/2023";
+
+const SECOND = 1000;
+const MINUTE = SECOND * 60;
+const HOUR = MINUTE * 60;
+const DAY = HOUR * 24;
+
+const Counter = () => {
+  const intervalRef = useRef(null);
+
+  const [remaining, setRemaining] = useState({
+    days: 0,
+    hours: 0,
+    minutes: 0,
+    seconds: 0,
+  });
+
+  useEffect(() => {
+    intervalRef.current = setInterval(handleCountdown, 1000);
+
+    return () => clearInterval(intervalRef.current || undefined);
+  }, []);
+
+  const handleCountdown = () => {
+    const end = new Date(COUNTDOWN_FROM);
+
+    const now = new Date();
+
+    const distance = +end - +now;
+
+    const days = Math.floor(distance / DAY);
+    const hours = Math.floor((distance % DAY) / HOUR);
+    const minutes = Math.floor((distance % HOUR) / MINUTE);
+    const seconds = Math.floor((distance % MINUTE) / SECOND);
+
+    setRemaining({
+      days,
+      hours,
+      minutes,
+      seconds,
+    });
+  };
+
   return (
-    <Countdown
-    date={Date.UTC(2023, 9, 21)}
-    intervalDelay={.8}
-    precision={3}
-    renderer={props => <div className=' animate-border inline-block rounded-md bg-white bg-gradient-to-r from-orange-600 shadow-md drop-shadow-md  shadow-orange-800 to-orange-900 bg-[length:400%_400%] p-[3px]'>
-        <div className='flex bg-black rounded-md'>
+      <div className="w-full max-w-5xl mx-auto flex items-center rounded-xl m-auto bg-transparent">
+        <CountdownItem num={remaining.days} text="days" />
+        <CountdownItem num={remaining.hours} text="hours" />
+        <CountdownItem num={remaining.minutes} text="minutes" />
+        <CountdownItem num={remaining.seconds} text="seconds" />
+      </div>
+  );
+};
 
-        <SingleFace>{props.days}</SingleFace>
-        <SingleFace>{props.hours}</SingleFace>
-        <SingleFace>{props.seconds}</SingleFace>
-        </div>
-    </div>}
-  />
-  )
-}
+const CountdownItem = ({ num, text }) => {
+  return (
+    <div className="font-mono w-1/4 h-24 md:h-36 flex flex-col gap-1 rounded-xl md:gap-2 items-center justify-center border-r-[1px] border-transparent last:border-none bg-[#ffd700]/20">
+      <div className="w-full text-center relative overflow-hidden">
+        <AnimatePresence mode="popLayout">
+          <motion.span
+            key={num}
+            initial={{ y: "100%" }}
+            animate={{ y: "0%" }}
+            exit={{ y: "-100%" }}
+            transition={{ ease: "backIn", duration: 0.75 }}
+            className="block text-2xl md:text-4xl lg:text-6xl xl:text-7xl text-black font-medium"
+          >
+            {num}
+          </motion.span>
+        </AnimatePresence>
+      </div>
+      <span className="text-xs md:text-sm lg:text-base font-light text-slate-500">
+        {text}
+      </span>
+    </div>
+  );
+};
 
-function SingleFace(props) {
-    return(
-        <div className='p-2 '>{props.children}</div>
-    );
-}
-
-
-export default Counter
+export default Counter;
--- src/Page/Home.jsx
+++ src/Page/Home.jsx
@@ -1,18 +1,23 @@
 import React from 'react'
-import { motion, useMotionTemplate, useMotionValue } from 'framer-motion'
-import NavBar from '../Components/NavBar'
-import Background from '../Components/Background'
+import { motion,  useScroll, useSpring } from 'framer-motion'
+
 import Counter from '../Components/Counter'
-import ParticleSystem from '../Components/ParticleSystem'
+
 
 function Home() {
   
+
+  
   return (
-    <div className="flex justify-items-center h-screen">
+    <div className="flex flex-col justify-items-center min-h-screen">
       
-      <div className='m-auto text-white flex'>
+      <div className='m-auto text-white flex h-screen' >
       {/* <Counter /> */}
-      <img src='/Logo512.png' className=' w-64 h-64 m-auto' />
+      <img src='/Logo512.png' className=' w-64 h-64 m-auto'/>
+      </div>
+      <div className='flex flex-col h-screen justify-items-center'>
+
+      <Counter />
       </div>
     </div>
 

d0e059f70ce41f094e9b71c7cfe571b60a489443
sudharaathith <t.sudharaathith@gmail.com>
2023-09-27 14:23:27 +0530

    summa

---
+++ src/Components/NavBar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function NavBar() {
+  return (
+    <div className=' border px-3 py-5 flex flex-row'>
+        hi
+    </div>
+  )
+}
+
+export default NavBar
--- src/Page/Home.jsx
+++ src/Page/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import { motion } from 'framer-motion'
+import NavBar from '../Components/NavBar'
 
 function Home() {
   return (
     <div className=' w-screen h-screen bg-[#181402]'>
+      <NavBar />
          
     </div>
   )

966ffb17d97199b0717ceb98de29cf28b74f90f7
sudharaathith <t.sudharaathith@gmail.com>
2023-09-27 14:38:30 +0530

    summa

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 
 function NavBar() {
   return (
-    <div className='  fixed top-0 w-screen bg-[#15120f]    px-3 py-5 flex flex-row'>
-        hi
+    <div className='  fixed top-0 w-screen bg-[#15120f] text-orange-700  px-3 py-5 flex flex-row'>
+        Cybrog
+        <div></div>
     </div>
   )
 }

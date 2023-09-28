0abd36e91abf980b2ada4bdc21543caeffdffa5f
sudharaathith <t.sudharaathith@gmail.com>
2023-09-28 10:26:39 +0530

    ui

--- src/Components/Background.jsx
+++ src/Components/Background.jsx
@@ -1,30 +1,22 @@
-import React from 'react'
-import { motion, useMotionTemplate, useMotionValue } from 'framer-motion'
+import React, { useState } from 'react'
+import { motion, useMotionTemplate } from 'framer-motion'
 import ParticleSystem from './ParticleSystem';
 import NavBar from './NavBar';
+import useMouse from '../Util/useMouse';
 
 function Background(props) {
-    let mouseX = useMotionValue(0);
-  let mouseY = useMotionValue(0);
+  const { handleMouseMove, mouseX, mouseY } = useMouse();
+  const [isHide, setIsHide] = useState(false);
 
-  function handleMouseMove({
-    currentTarget,
-    clientX,
-    clientY,
-  }) {
-    let { left, top } = currentTarget.getBoundingClientRect();
-
-    mouseX.set(clientX - left);
-    mouseY.set(clientY - top);
-  }
+  setTimeout(() => setIsHide(true), 6000);
   return (
     <div
-      className="group bg-black w-full min-h-screen "
+      className="group absolute bg-black w-screen min-h-screen overflow-x-hidden"
       onMouseMove={handleMouseMove}
     >
       
         <motion.div
-          className="pointer-events-none absolute inset-0 rounded-xl transition duration-300 opacity-100"
+          className="pointer-events-none top-0 left-0 w-screen h-full absolute inset-0 rounded-xl transition duration-300 opacity-100 overflow-x-hidden"
           style={{
             background: useMotionTemplate`
               radial-gradient(
@@ -35,8 +27,8 @@ function Background(props) {
             `,
           }}
         />
-        <ParticleSystem />
-        <div className={props.className}>
+        {isHide?<ParticleSystem />:null}
+        <div className="">
         <NavBar />
          { props.children }
         </div>

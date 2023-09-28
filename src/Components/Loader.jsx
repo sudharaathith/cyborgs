97f617ad734891c8f384316d264724042ec0f987
sudharaathith <t.sudharaathith@gmail.com>
2023-09-28 18:48:51 +0530

    l

--- src/Components/Loader.jsx
+++ src/Components/Loader.jsx
@@ -1,10 +1,10 @@
 import React, { useEffect, useState } from 'react'
-import image from '../Image/Cyborgs.gif'
+import image from '../Image/Cyborgs1.gif'
 
 function Loader({children}) {
     
   const[show, setShow]=useState(true);
-  useEffect(()=>{setTimeout(()=>{setShow(false)},5000)},[])
+  useEffect(()=>{setTimeout(()=>{setShow(false)},2500)},[])
   return (
     <>
     {show?<img className='fixed top-0 left-0 w-screen h-screen z-50' src={image} alt="My Image" />:children}

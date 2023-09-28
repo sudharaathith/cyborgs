0d75e7a5c25edbfb21f9381b164b87f9df462e4c
Bharath Gopalswamy <110247438+bharathgopalswamy@users.noreply.github.com>
2023-09-28 21:43:22 +0530

    responsive,animated about section

--- src/Page/AboutSection.jsx
+++ src/Page/AboutSection.jsx
@@ -17,17 +17,6 @@ function AboutSection() {
     },
   };
 
-  const imageVariants = {
-    hidden: { opacity: 0, scale: 0.8 },
-    visible: {
-      opacity: 1,
-      scale: 1,
-      transition: {
-        duration: 0.5,
-      },
-    },
-  };
-
   return (
     <motion.div
       initial="hidden"
@@ -36,13 +25,30 @@ function AboutSection() {
       className="m-auto text-white flex"
     >
       <div className="bg-transparent text-white py-8">
-        <div className="container mx-auto px-4 flex flex-wrap mt-[10%] ml-[10%] items-center text-justify ">
-          <h2 className="text-3xl text-center font-bold w-full mb-4 ">Overview</h2>
+      <div className="container mx-auto px-14 ml-6 py-2 md:py-6 lg:py-[10%] flex flex-wrap items-center justify-center text-justify">
 
-          <div className="w-full md:w-2/5 mb-4">
+          <h2 className="text-3xl text-center font-bold w-full mb-4">Overview</h2>
+
+          <style>
+            {`
+              .shaggy-image {
+                animation: shaggy 2s infinite alternate;
+              }
+
+              @keyframes shaggy {
+                0% {
+                  transform: translateY(0);
+                }
+                100% {
+                  transform: translateY(-10px);
+                }
+              }
+            `}
+          </style>
+
+          <div className="w-full md:w-1/2 lg:w-2/5 mb-4">
             <motion.div
-              className="pics border rounded-full border-white overflow-hidden"
-              variants={imageVariants}
+              className="pics border rounded-full border-white overflow-hidden shaggy-image"
             >
               <img
                 src="https://tneacounselling.in/wp-content/uploads/2022/08/SRM-Valliammai-Engineering-College-Kattankulathur-Chengalpattu.jpg"
@@ -52,22 +58,21 @@ function AboutSection() {
             </motion.div>
           </div>
 
-          <div className="w-full md:w-3/5 mb-4">
-            <p className="text-lg mb-4 ml-[2rem]">
+          <div className="w-full md:w-1/2 lg:w-3/5 mb-6">
+            <p className="text-lg mb-4 ml-0 md:ml-8">
               SRM Valliammai Engineering College (An Autonomous Institution) was established on September 9, 1999, and presently conducts 11 Undergraduate courses and 8 Postgraduate courses. The college has highly qualified, dynamic, and dedicated renowned faculty both from academic and industrial backgrounds. Besides, the students bring laurels to the college by securing university ranks.
             </p>
           </div>
 
-          <div className="w-full md:w-1/2 mb-4">
-            <p className="text-lg mb-4 mr-[2rem]">
+          <div className="w-full md:w-1/2 lg:w-1/2 mb-4">
+            <p className="text-lg mb-4 mr-0 md:mr-8">
               It is situated on an expansive 13.35-acre campus, adjacent to NH 45, between Tambaram and Chengalpattu. The serene atmosphere with lush green surroundings is conducive to the pursuit of higher studies.
             </p>
           </div>
 
-          <div className="w-full md:w-2/5 mb-4">
+          <div className="w-full md:w-1/2 lg:w-2/5 mb-4">
             <motion.div
-              className="pics border rounded-full border-white overflow-hidden"
-              variants={imageVariants}
+              className="pics border rounded-full border-white overflow-hidden shaggy-image"
             >
               <img
                 src="https://tneacounselling.in/wp-content/uploads/2022/08/SRM-Valliammai-Engineering-College-Kattankulathur-Chengalpattu.jpg"
@@ -77,10 +82,9 @@ function AboutSection() {
             </motion.div>
           </div>
 
-          <div className="w-full md:w-2/5 mb-4">
+          <div className="w-full md:w-1/2 lg:w-2/5 mb-4">
             <motion.div
-              className="pics border rounded-full border-white overflow-hidden"
-              variants={imageVariants}
+              className="pics border rounded-full border-white overflow-hidden shaggy-image"
             >
               <img
                 src="https://tneacounselling.in/wp-content/uploads/2022/08/SRM-Valliammai-Engineering-College-Kattankulathur-Chengalpattu.jpg"
@@ -90,22 +94,21 @@ function AboutSection() {
             </motion.div>
           </div>
 
-          <div className="w-full md:w-1/2 mb-4">
-            <p className="text-lg mb-4 ml-[2rem] ">
+          <div className="w-full md:w-1/2 lg:w-1/2 mb-4">
+            <p className="text-lg mb-4 ml-0 md:ml-8">
               The college operates from a multi-story building complex covering an impressive 59,000 square meters to cater to the demands of an ever-increasing number of students.
             </p>
           </div>
 
-          <div className="w-full md:w-3/5 mb-4">
-            <p className="text-lg mb-4 mr-[2rem]">
+          <div className="w-full md:w-1/2 lg:w-3/5 mb-4">
+            <p className="text-lg mb-4 mr-0 md:mr-8">
               SRM Valliammai Engineering College is a prestigious part of the SRM Group of Educational Institutions, sponsored by the Valliammai Society. The Valliammai Society was founded by Dr.T.R.Paarivendhar, a well-known educationist, in honor of his mother Tmt. R.Valliammai. Her care and guidance played a significant role in his personal development, and her ideals continue to inspire him.
             </p>
           </div>
 
-          <div className="w-full md:w-2/5 mb-4">
+          <div className="w-full md:w-1/2 lg:w-2/5 mb-4">
             <motion.div
-              className="pics border rounded-full border-white overflow-hidden"
-              variants={imageVariants}
+              className="pics border rounded-full border-white overflow-hidden shaggy-image"
             >
               <img
                 src="https://tneacounselling.in/wp-content/uploads/2022/08/SRM-Valliammai-Engineering-College-Kattankulathur-Chengalpattu.jpg"
